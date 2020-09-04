3ea37a719ebcadd47cbc11b61baca5dc1cfaf756
Logan Blackstad <logan.blackstad@gmail.com>
2020-09-04 17:21:44 -0400

    added Users component

---
+++ react-sandbox/react-sandbox/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Users from 'components/Users.js';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Users name="Sara">
+    </div>
+  );
+}
+
+export default App;
---
+++ react-sandbox/react-sandbox/src/components/Users.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export const Component = (props) => {
+  return (
+    <div>
+      {props.children}
+    </div>
+  )
+}
+
+export default Component;
